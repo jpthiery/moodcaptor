1c3945236669bd9c9599d40f0747415c472e60e5
jpthiery <jpthiery@xebia.fr>
2021-03-19 21:37:03 +0100

    ✨  Allow to build several function in one repository

---
+++ moodcaptor-backend/handler.py
@@ -0,0 +1,16 @@
+def handle(req):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+
+    return req
+
+
+def handle_bis(req):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+
+    return "Coucou"
